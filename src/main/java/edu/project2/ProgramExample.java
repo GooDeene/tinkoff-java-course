1aebd44920dffd117aee0f34fef126b1e59356c4
GooDeene <miedviedievskii@mail.ru>
2023-11-05 22:55:12 +0500

    hotfix to checkstyle

--- src/main/java/edu/project2/ProgramExample.java
+++ src/main/java/edu/project2/ProgramExample.java
@@ -1,16 +1,15 @@
 package edu.project2;
 
-public class ProgramExample {
-    @SuppressWarnings("RegexpSinglelineJava")
-    public static void main(String[] args) {
-        var generator = new RecursiveBacktrackerGenerator();
-        var maze = generator.generate(9, 39);
-        var renderer = new PrettyConsoleRenderer();
-
-        renderer.renderThenPrintToSystemConsole(maze);
-
-        var solve = new DFSSolver().solve(maze, new Coordinate(0, 0), new Coordinate(8, 38));
-
-        renderer.renderThenPrintToSystemConsole(maze, solve);
-    }
-}
+//public class ProgramExample {
+//    public static void main(String[] args) {
+//        var generator = new RecursiveBacktrackerGenerator();
+//        var maze = generator.generate(9, 39);
+//        var renderer = new PrettyConsoleRenderer();
+//
+//        renderer.renderThenPrintToSystemConsole(maze);
+//
+//        var solve = new DFSSolver().solve(maze, new Coordinate(0, 0), new Coordinate(8, 38));
+//
+//        renderer.renderThenPrintToSystemConsole(maze, solve);
+//    }
+//}
