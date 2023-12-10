1280d73e0ac37e698415c40b755793de4fcfbf2d
GooDeene <miedviedievskii@mail.ru>
2023-12-11 01:56:35 +0500

    test fix

--- src/test/java/edu/hw9/StatisticCollectorTest.java
+++ src/test/java/edu/hw9/StatisticCollectorTest.java
@@ -1,8 +1,8 @@
 package edu.hw9;
 
 import edu.hw9.task1.Metric;
-import java.util.concurrent.Executors;
 import edu.hw9.task1.StatisticCollector;
+import java.util.concurrent.Executors;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -13,10 +13,10 @@ public class StatisticCollectorTest {
         var collector = new StatisticCollector(2);
         var executorService = Executors.newFixedThreadPool(2);
 
-        executorService.execute(() -> collector.push("metric1", new double[] { 1 }));
-        executorService.execute(() -> collector.push("metric2", new double[] { 1, 4, 3, 4, 5 }));
-        executorService.execute(() -> collector.push("metric3", new double[] { 1, 9, 3, 4, 5 }));
-        executorService.execute(() -> collector.push("metric4", new double[] { 0, 44, 3, 4, 5} ));
+        executorService.execute(() -> collector.push("metric1", new double[] {1}));
+        executorService.execute(() -> collector.push("metric2", new double[] {1, 4, 3, 4, 5}));
+        executorService.execute(() -> collector.push("metric3", new double[] {1, 9, 3, 4, 5}));
+        executorService.execute(() -> collector.push("metric4", new double[] {0, 44, 3, 4, 5}));
 
         var stats = collector.stats();
         Assertions.assertThat(stats).containsExactlyInAnyOrder(
